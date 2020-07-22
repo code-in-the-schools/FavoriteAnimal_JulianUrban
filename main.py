f59881058debe11424631506a7cd42109e912918
thejduman <thejduman@yahoo.com>
2020-07-22 14:58:13 +0000

    Finished the program

---
+++ main.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+if num == 1:
+  print("Polar bears have a layer of body fat and a water-repellant coat that acts as insulation")
+elif num == 2:
+  print("Polar bears spend 50% of their time hunting for food")
+elif num == 3:
+  print("Sea habitats for polar bears are declining")
+elif num == 4:
+  print("Polar bears can swim at 6 miles per hour")
+else:
+  print("Invalid number")
